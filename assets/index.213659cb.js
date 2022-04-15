c07b1f14402393d49c4f9be283bcacb4b08193ea
huangbeizhuang <1109646615@qq.com>
2022-04-15 11:02:18 +0800

    提交代码

---
+++ assets/index.213659cb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.b0fcde14.js";import"./vendor.f6a25964.js";const r={created(){console.log(this.$route)}};function t(o,s,c,n,a,_){return" my "}var d=e(r,[["render",t]]);export{d as default};
